4ef8998f0fc57a19564cb2ed85a58fe3359fce09
02-ahmed <ahmedboabae2@gmail.com>
2024-06-22 13:50:43 +0000

    implemented a completed feature

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import "./App.css"
 interface Todo {
   id: number,
   todo: string,
-  dueDate: Date
+  dueDate: Date,
+  completed: boolean
 }
 
 function loadTodosFromLocalStorage(): Todo[] {
@@ -15,7 +16,8 @@ function loadTodosFromLocalStorage(): Todo[] {
     try {
       return JSON.parse(savedTodos).map((todo: Todo) => ({
         ...todo,
-        dueDate: new Date(todo.dueDate) // Convert dueDate string back to a Date object
+        dueDate: new Date(todo.dueDate), // Convert dueDate string back to a Date object
+        completed: todo.completed||false
       }));
     } catch (error) {
       console.error("Failed to parse todos from local storage:", error);
@@ -23,25 +25,37 @@ function loadTodosFromLocalStorage(): Todo[] {
     }
   }
   return [];
-}
+};
+
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>(() => loadTodosFromLocalStorage());
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos))
-  }, [todos])
+  }, [todos]);
+
+  const toggleCompletion = (id:number) => {
+    setTodos(todos.map(todo => 
+      todo.id === id? {...todo, completed:!todo.completed}:todo
+    ));
+  }
+
   return (
     <>
     <div className="text-center heading">
       <h1>Tasky</h1>
     </div>
     <div className="mb-5 mt-5">
-      <AddTodo onSubmit={(todo) => setTodos(([...todos, {...todo, id:todos.length + 1}]))}/>
+      <AddTodo onSubmit={(todo) => setTodos(([...todos, {...todo, id:todos.length + 1, completed:false}]))}/>
     </div>
 
     <div>
-      {todos.length > 0 && <TodoList todos={todos} onDelete={(id) => setTodos(todos.filter((todo) => todo.id !== id))}/>}
+      {todos.length > 0 && <TodoList 
+      todos={todos}
+      onDelete={(id) => setTodos(todos.filter((todo) => todo.id !== id))}
+      onDone={toggleCompletion}
+      />}
     </div>
       
       
